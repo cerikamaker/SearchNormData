7d8db75d48545e80a15de923500dce7ee42accee
cerikamaker <team@searchnorm.de>
2025-09-21 11:01:12 +0200

    Initial commit

---
+++ target/debug/deps/sn_directives-0996ef2d25f05b76.d
@@ -0,0 +1,5 @@
+D:\Büro\Entwicklung\Datenmodel\target\debug\deps\sn_directives-0996ef2d25f05b76.d: sn_directives\src\lib.rs
+
+D:\Büro\Entwicklung\Datenmodel\target\debug\deps\libsn_directives-0996ef2d25f05b76.rmeta: sn_directives\src\lib.rs
+
+sn_directives\src\lib.rs:
