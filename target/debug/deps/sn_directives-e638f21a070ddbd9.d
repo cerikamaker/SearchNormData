31dba06c7b64f0cdf73ba8341313e53606b8db86
cerikamaker <team@searchnorm.de>
2025-09-21 11:08:43 +0200

    Cleaning2

--- target/debug/deps/sn_directives-e638f21a070ddbd9.d
+++
@@ -1,5 +0,0 @@
-D:\Büro\Entwicklung\Datenmodel\target\debug\deps\sn_directives-e638f21a070ddbd9.d: sn_directives\src\lib.rs
-
-D:\Büro\Entwicklung\Datenmodel\target\debug\deps\libsn_directives-e638f21a070ddbd9.rmeta: sn_directives\src\lib.rs
-
-sn_directives\src\lib.rs:
--- target/debug/deps/sn_products-ee2b31288912209b.d
+++
@@ -1,5 +0,0 @@
-D:\Büro\Entwicklung\Datenmodel\target\debug\deps\sn_products-ee2b31288912209b.d: sn_products\src\lib.rs
-
-D:\Büro\Entwicklung\Datenmodel\target\debug\deps\libsn_products-ee2b31288912209b.rmeta: sn_products\src\lib.rs
-
-sn_products\src\lib.rs:
